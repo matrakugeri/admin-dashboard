6ff9a965d6b0f8b60f3cd6982115f1bae7c4eabe
matrakugeri <matrakugeri@gmail.com>
2025-07-19 13:25:39 +0200

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin-dashboard',
+    loadComponent: () =>
+      import('./pages/admin-dashboard/admin-dashboard.component').then(
+        (m) => m.AdminDashboardComponent
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'admin-dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'admin-dashboard',
+  },
+];
